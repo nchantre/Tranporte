5bb73ade68f4ef4ce0e8366cd66d7acf0cbb04cd
nchantre <nelsonfabiancs@hotmail.com>
2023-02-01 19:23:48 -0500

    ajustes
    Ajustes

--- transporteFronend/Transporte/src/app/app.module.ts
+++ transporteFronend/Transporte/src/app/app.module.ts
@@ -2,17 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavigationComponent } from './components/navigation/navigation.component';
+import { NavigationComponent } from './transito/navigation/navigation.component';
+import { AppRoutingModule } from './app-routing.module';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
---
+++ transporteFronend/Transporte/src/app/transito/transito.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+
+@NgModule({
+  declarations: [
+
+  ],
+  exports:[
+
+  ],
+  imports: [
+    CommonModule
+  
+  ]
+})
+export class TransitoModule { }
